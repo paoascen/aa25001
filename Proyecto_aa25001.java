56527462102913606755fb89fc0d5a13780ba381
aa25001 <aa25001@ues.edu.sv>
2025-05-11 21:37:23 -0400

    My first commit

---
+++ Proyecto_aa25001.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        String carnet = "aa25001"; // Tu número de carnet
+        System.out.println("Mi carnet estudiantil es: " + carnet);
+    }
+}
